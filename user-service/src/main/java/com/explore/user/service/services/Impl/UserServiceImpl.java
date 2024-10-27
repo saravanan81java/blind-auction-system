da12cbb5770f91a6ebcf3a9cd005b4cd7013b710
Saravanan <saran_jad@yahoo.co.in>
2024-10-27 03:08:20 +0000

    Added new changes

--- user-service/src/main/java/com/explore/user/service/services/Impl/UserServiceImpl.java
+++ user-service/src/main/java/com/explore/user/service/services/Impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package com.explore.user.service.services.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import com.explore.user.service.entity.User;
 import com.explore.user.service.repository.UserRepository;
 import com.explore.user.service.services.UserService;
 
+@Service
 public class UserServiceImpl implements UserService{
 	
 	@Autowired
