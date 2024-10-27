4b5978575bc3f710b0a1dac007ba2692d035841a
Saravanan <saran_jad@yahoo.co.in>
2024-10-27 03:31:30 +0000

    added prop

--- auction-service/src/main/java/com/explore/auction/service/repository/BidRepository.java
+++ auction-service/src/main/java/com/explore/auction/service/repository/BidRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.explore.auction.service.entity.Bid;
 
 public interface BidRepository extends JpaRepository<Bid, Long> {
-	List<Bid> findByAuctionIdOrderByAmountDesc(Long auctionId);
+	//List<Bid> findByAuctionIdOrderByAmountDesc(Long auctionId);
 }
